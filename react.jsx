098c012993a7405852261eaa0c5096bd1c3747a0
suhaibsidd138 <suhaibsidd9927@gmail.com>
2025-02-07 15:32:34 +0530

    Create react.jsx

---
+++ react.jsx
@@ -0,0 +1,19 @@
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Projects from '../components/Projects';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
